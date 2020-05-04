186a1ad2ba2ff5b26f90f6aebd4c1b77ef6d7866
Bruno Queiroz <bruno.queiroz2011@gmail.com>
2020-05-04 16:46:25 -0300

    CORS

--- backend/server/cors.ts
+++ backend/server/cors.ts
@@ -3,7 +3,9 @@ import * as restify from 'restify'
 export const cors = (req, resp, next) =>{
     resp.setHeader('Access-Control-Allow-Origin', '*');
     resp.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
-    resp.setHeader('Access-Control-Allow-Headers', 'Origin , X-Requested-With, Content-Type, Accept');
-    resp.setHeader('Access-Control-Allow-Credentials', "true");
+    resp.setHeader('Access-Control-Allow-Headers', 'Origin ,Authorization, X-Requested-With, Content-Type, Accept');
+    resp.setHeader('Access-Control-Allow-Credentials', 'true');
+    resp.setHeader('Content-type', 'application/json');
+
     return next()
   }
